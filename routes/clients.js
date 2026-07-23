cfc5608ee80357f86ae9079ae918071c4df68688
nkpautomationai-alt <nkpautomation.ai@gmail.com>
2026-07-24 02:19:07 +0530

    Update clients.js

--- routes/clients.js
+++ routes/clients.js
@@ -3,9 +3,13 @@ const router = express.Router();
 const { search } = require("../controllers/clientController");
 const { createClient } = require("../controllers/clientController");
 const { getClient } = require("../controllers/clientController");
+const { getActivity } = require("../controllers/clientController");
+const { getAuditLog } = require("../controllers/clientController");
 
 router.post("/create", createClient);
 router.get("/search", search);
 router.get("/client", getClient);
+router.get("/activity", getActivity);
+router.get("/audit-log", getAuditLog);
 
 module.exports = router;
